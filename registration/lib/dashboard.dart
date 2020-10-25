241b5f34c79675434e33e04a404b681e28377570
Jan Mewes <jan.mewes@experimental-software.com>
2020-10-25 13:59:14 +0100

    WIP  [ci skip]

--- registration/lib/dashboard.dart
+++ registration/lib/dashboard.dart
@@ -3,8 +3,31 @@ import 'package:flutter/material.dart';
 class RegistrationDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Dashboard'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            'Dashboard',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 35,
+            ),
+          ),
+        ),
+        Ink(
+          color: Colors.yellow,
+          width: 200.0,
+          height: 100.0,
+          child: InkWell(
+              onTap: () { },
+              child: Center(
+                child: Text('YELLOW'),
+              )
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ registration/lib/dashboard.example.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:registration/dashboard.dart';
+
+import 'test_bench.dart';
+
+void main() {
+  runApp(
+    TestBench(
+      RegistrationDashboard()
+    ),
+  );
+}
