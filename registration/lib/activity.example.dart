b05add8340faba231257a1d355d1b06b509a16f0
Jan Mewes <jan.mewes@experimental-software.com>
2020-10-25 18:15:53 +0100

    WIP  [ci skip]

--- registration/lib/activity.example.dart
+++ registration/lib/activity.example.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:registration/activity.dart';
-
-import 'test_bench.dart';
+import 'package:registration/test_bench.dart';
 
 void main() {
   runApp(
     TestBench(
       child: Activity(
         title: "Demo",
-        body: Text("Hello, World!"),
-        backButtonCallback: () => print("TODO: Navigate to previous screen."),
+        body: Text("Hello, World!!"),
       ),
     ),
   );
