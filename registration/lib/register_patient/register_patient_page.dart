8ec872d8ed63825f0979779d00dfe9b45d286103
Jan Mewes <jan.mewes@epages.com>
2020-11-21 16:11:14 +0100

    WIP

--- registration/lib/register_patient/register_patient_page.dart
+++ registration/lib/register_patient/register_patient_page.dart
@@ -45,50 +45,52 @@ class _RegisterPatientPageState extends State<RegisterPatientPage> {
           ),
           Row(
             children: [
-              DataTable(
-                columns: const <DataColumn>[
-                  DataColumn(
-                    label: Text(
-                      'Name',
-                      style: TextStyle(fontStyle: FontStyle.italic),
+              Expanded(
+                child: DataTable(
+                  columns: const <DataColumn>[
+                    DataColumn(
+                      label: Text(
+                        'Name',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
                     ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Age',
-                      style: TextStyle(fontStyle: FontStyle.italic),
+                    DataColumn(
+                      label: Text(
+                        'Age',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
                     ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Role',
-                      style: TextStyle(fontStyle: FontStyle.italic),
+                    DataColumn(
+                      label: Text(
+                        'Role',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
                     ),
-                  ),
-                ],
-                rows: const <DataRow>[
-                  DataRow(
-                    cells: <DataCell>[
-                      DataCell(Text('Sarah')),
-                      DataCell(Text('19')),
-                      DataCell(Text('Student')),
-                    ],
-                  ),
-                  DataRow(
-                    cells: <DataCell>[
-                      DataCell(Text('Janine')),
-                      DataCell(Text('43')),
-                      DataCell(Text('Professor')),
-                    ],
-                  ),
-                  DataRow(
-                    cells: <DataCell>[
-                      DataCell(Text('William')),
-                      DataCell(Text('27')),
-                      DataCell(Text('Associate Professor')),
-                    ],
-                  ),
-                ],
+                  ],
+                  rows: const <DataRow>[
+                    DataRow(
+                      cells: <DataCell>[
+                        DataCell(Text('Sarah')),
+                        DataCell(Text('19')),
+                        DataCell(Text('Student')),
+                      ],
+                    ),
+                    DataRow(
+                      cells: <DataCell>[
+                        DataCell(Text('Janine')),
+                        DataCell(Text('43')),
+                        DataCell(Text('Professor')),
+                      ],
+                    ),
+                    DataRow(
+                      cells: <DataCell>[
+                        DataCell(Text('William')),
+                        DataCell(Text('27')),
+                        DataCell(Text('Associate Professor')),
+                      ],
+                    ),
+                  ],
+                ),
               )
             ],
           ),
