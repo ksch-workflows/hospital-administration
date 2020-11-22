e50137e8b7de991440ac01da5ce7f29de6da4557
Jan Mewes <jan.mewes@epages.com>
2020-11-22 20:28:11 +0100

    WIP

--- registration/test/register_patient/register_patient_page_test.dart
+++ registration/test/register_patient/register_patient_page_test.dart
@@ -1,19 +1,10 @@
-/*
-TODO
-- should show search results
-- should not show search result table before the search
-- should show message about empty search results
-- should open dialog for patient registration
-- should navigate to patient details
- */
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:registration/register_patient/register_patient_page.dart';
-import 'package:registration/test_bench.dart';
+import "package:flutter/material.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:registration/register_patient/register_patient_page.dart";
+import "package:registration/test_bench.dart";
 
 void main() {
-  testWidgets('Should show search results', (WidgetTester tester) async {
+  testWidgets("Should show search results", (WidgetTester tester) async {
     tester.binding.window.textScaleFactorTestValue = 0.2;
 
     await tester.pumpWidget(TestBench(
@@ -26,5 +17,31 @@ void main() {
     await tester.pump();
 
     expect(find.byType(DataTable), findsOneWidget);
+  });
+
+  testWidgets("Should not show search result table before the search", (WidgetTester tester) async {
+    tester.binding.window.textScaleFactorTestValue = 0.2;
+
+    await tester.pumpWidget(TestBench(
+      child: RegisterPatientPage(),
+      isFullPage: false,
+    ));
+
+    expect(find.byType(DataTable), findsNothing);
+  });
+
+  testWidgets("Should show message about no search results", (WidgetTester tester) async {
+    tester.binding.window.textScaleFactorTestValue = 0.2;
+
+    await tester.pumpWidget(TestBench(
+      child: RegisterPatientPage(),
+      isFullPage: false,
+    ));
+
+    var patientSearchInputField = find.byType(TextField);
+    await tester.enterText(patientSearchInputField, "xxxxxxx");
+    await tester.pump();
+
+    expect(find.text("No patients found."), findsOneWidget);
   });
 }
