59cfe68ee8b51f1a7fe6a9ed4b6d9efdf618be25
Jan Mewes <jan.mewes@experimental-software.com>
2020-12-21 18:30:18 +0100

    WIP  [ci skip]

--- registration/lib/ui/register_patient/register_patient_dialog/visit_type.dart
+++ registration/lib/ui/register_patient/register_patient_dialog/visit_type.dart
@@ -32,34 +32,58 @@ class _VisitTypeFormStepBody extends StatefulWidget {
 class _VisitTypeFormStepBodyState extends State<_VisitTypeFormStepBody> {
   final FormValue<String> visitTypeSelection;
 
-  _VisitTypeFormStepBodyState({@required this.visitTypeSelection}): assert(visitTypeSelection != null);
+  _VisitTypeFormStepBodyState({@required this.visitTypeSelection}) : assert(visitTypeSelection != null);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        DropdownButton<String>(
-          value: visitTypeSelection.value,
-          icon: Icon(Icons.arrow_downward),
-          iconSize: 24,
-          elevation: 16,
-          style: TextStyle(color: Colors.deepPurple),
-          underline: Container(
-            height: 2,
-            color: Colors.deepPurpleAccent,
-          ),
-          onChanged: (String newValue) {
-            setState(() {
-              visitTypeSelection.value = newValue;
-            });
-          },
-          items: <String>['One', 'Two', 'Free', 'Four']
-              .map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
+        Row(
+          children: [
+            Expanded(
+              child: InputDecorator(
+                decoration: InputDecoration(
+                  labelText:  visitTypeSelection.value != null ? "Visit type" : null,
+                  border: const OutlineInputBorder(),
+                  contentPadding: EdgeInsets.only(left: 10)
+                  //contentPadding: EdgeInsets.only(left: 10),
+                ),
+                child: Container(
+                  // padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  // decoration: BoxDecoration(
+                  //   borderRadius: BorderRadius.circular(3),
+                  //   border: Border.all(
+                  //     color: Colors.grey[500],
+                  //   ),
+                  // ),
+                  // decoration: BoxDecoration(
+                  //
+                  //     border: Border.all()),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      value: visitTypeSelection.value,
+                      icon: Icon(Icons.arrow_downward),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: TextStyle(color: Colors.deepPurple),
+                      hint: Text("Please select a visit type..."),
+                      onChanged: (String newValue) {
+                        setState(() {
+                          visitTypeSelection.value = newValue;
+                        });
+                      },
+                      items: <String>['OPD', 'IPD'].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ],
     );
